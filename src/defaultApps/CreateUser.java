b3c03055f6cce9129baa939f8b30f8b733ca8cc9
ThePerkinrex <theperkinrex@gmail.com>
2017-04-05 19:42:01 +0200

    Add a create user class

---
+++ src/defaultApps/CreateUser.java
@@ -0,0 +1,25 @@
+package defaultApps;
+
+import processing.core.PApplet;
+import window.Window;
+
+public class CreateUser extends Window {
+
+	public CreateUser(PApplet sketch) {
+		super(sketch, 400, 600, 100, 100, "Create User");
+	}
+
+	@Override
+	protected void updateApp() {
+	}
+
+	@Override
+	protected void displayApp() {
+		//sketch.rect(x,gy,width,height);
+	}
+	
+	@Override
+	public void setup() {
+	}
+
+}
